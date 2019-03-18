61352316762eff56e0100287e5c2c5fe282ded2c
Jesse Wicks <jessewicks@gmail.com>
2019-03-17 23:22:24 -0400

    format

--- back-end/src/middleware/index.js
+++ back-end/src/middleware/index.js
@@ -1,24 +1,28 @@
-import { Router } from 'express';
+import {
+  Router
+} from 'express';
 
-export default ({ config }) => {
-	// eslint-disable-next-line new-cap
-	const routes = Router();
+export default ({
+  config
+}) => {
+  // eslint-disable-next-line new-cap
+  const routes = Router();
 
-	// add middleware here
-	routes.use((req, res, next) => {
-		// check header for the token
-		const token = req.headers['access-token'];
-		if (token) {
-			if (token !== config.secret) {
-				return res.json({
-					message: '401 Not Authorized'
-				});
-			} else next();
-		} else {
-			res.send({
-				message: '401 Not Authorized'
-			});
-		}
+  // add middleware here
+  routes.use((req, res, next) => {
+    // check header for the token
+    const token = req.headers['access-token'];
+    if (token) {
+      if (token !== config.secret) {
+        return res.json({
+          message: '401 Not Authorized'
+        });
+      } else next();
+    } else {
+      res.send({
+        message: '401 Not Authorized'
+      });
+    }
   });
-	return routes;
+  return routes;
 };
